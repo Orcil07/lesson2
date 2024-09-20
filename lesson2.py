853fcfc5a72a6ac38756348885c2ec767d7c6fef
Orcil07 <orcil9675@gmail.com>
2024-09-20 21:32:34 +0300

    Add files via upload

---
+++ lesson2.py
@@ -0,0 +1,8 @@
+course_name = "Python"
+print ("Курс:", course_name)
+task_count = 12
+print ("всего задач:",task_count)
+hour_count = 1.5
+print ("затрачено часов:", hour_count)
+average_time = 0.125
+print ("среднее время выполнения", average_time, "часа.")
